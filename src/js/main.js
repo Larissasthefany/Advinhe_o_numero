b135d42a79bba184ae770e84a45af3eefd0cbcca
Larissasthefany <larigithub@gmail.com>
2025-04-04 18:45:24 -0300

    feat: adiciona lógica de início do jogo e validação do input

---
+++ src/js/main.js
@@ -0,0 +1,49 @@
+import {
+  title,
+  player,
+  inputName,
+  btnStart,
+  feedback,
+  number,
+  btnSend,
+  message,
+  btnRoot,
+  mostrarJogo,
+  reiniciarJogo,
+} from "./ui.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  title.innerText = `Jogo de adivinhação`;
+  player.innerText = "Digite seu nome para começar o jogo:";
+  btnStart.innerText = "Iniciar Jogo";
+  btnSend.innerText = "Enviar";
+  btnRoot.innerText = "Reiniciar Jogo";
+});
+
+btnStart.addEventListener("click", () => {
+  if (inputName.value === "") {
+    feedback.innerText = "Digite seu nome para começar o jogo!";
+    feedback.classList.add("error");
+    feedback.classList.remove("success");
+    feedback.style.display = "block";
+    return;
+  } else {
+    feedback.innerText = `Olá ${inputName.value}, boa sorte!`;
+    feedback.classList.remove("error");
+    feedback.classList.add("success");
+    mostrarJogo();
+  }
+});
+
+btnSend.addEventListener("click", () => {
+  const numero = parseInt(number.value);
+
+  if (isNaN(numero) || numero < 1 || numero > 50) {
+    feedback.innerText = "Digite um número entre 1 e 50!";
+    feedback.classList.add("error");
+    feedback.classList.remove("success");
+    return;
+  }
+});
+
+btnRoot.addEventListener("click", reiniciarJogo);
