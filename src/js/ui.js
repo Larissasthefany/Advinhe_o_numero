85f502260e423fedad2eb5eccfdc869c8055f102
Larissasthefany <larigithub@gmail.com>
2025-04-04 18:39:12 -0300

    feat: adiciona controle da interface e funções de exibição do jogo

---
+++ src/js/ui.js
@@ -0,0 +1,33 @@
+export const title = document.getElementById("title");
+export const player = document.getElementById("player");
+export const inputName = document.getElementById("inputName");
+export const btnStart = document.getElementById("start");
+export const feedback = document.getElementById("feedback");
+export const number = document.getElementById("inputNumber");
+export const btnSend = document.getElementById("send");
+export const message = document.getElementById("messageGuess");
+export const btnRoot = document.getElementById("root");
+
+export function mostrarJogo() {
+  inputName.style.display = "none";
+  btnStart.style.display = "none";
+  player.style.display = "none";
+
+  number.style.display = "block";
+  btnSend.style.display = "block";
+  message.style.display = "block";
+  btnRoot.style.display = "block";
+  feedback.style.display = "none";
+}
+
+export function reiniciarJogo() {
+  inputName.style.display = "block";
+  btnStart.style.display = "block";
+  player.style.display = "block";
+
+  number.style.display = "none";
+  btnSend.style.display = "none";
+  message.style.display = "none";
+  btnRoot.style.display = "none";
+  feedback.style.display = "none";
+}
